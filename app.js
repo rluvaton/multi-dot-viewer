944e4af01aecda00c9700c894fd76719ff786377
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2025-06-19 11:58:21 +0300

    the mask at the rounding corners are not the same radius like the container:

--- app.js
+++ app.js
@@ -274,15 +274,16 @@ class MultiDotViewer {
     // Create a clipping mask for the header with rounded top corners only
     // Account for border width so mask stays inside container border
     const borderWidth = 1;
-    const innerWidth = diagramData.size.width - (borderWidth * 2);
+    const containerRadius = 12;
+    const innerRadius = containerRadius - borderWidth; // 11px inner radius
     const maskId = `header-mask-${diagramData.id}`;
     container.append('defs').append('clipPath')
       .attr('id', maskId)
       .append('path')
-      .attr('d', `M ${borderWidth},${12 - borderWidth} 
-                        Q ${borderWidth},${borderWidth} ${12},${borderWidth} 
-                        L ${diagramData.size.width - 12},${borderWidth} 
-                        Q ${diagramData.size.width - borderWidth},${borderWidth} ${diagramData.size.width - borderWidth},${12 - borderWidth} 
+      .attr('d', `M ${borderWidth},${borderWidth + innerRadius} 
+                        Q ${borderWidth},${borderWidth} ${borderWidth + innerRadius},${borderWidth} 
+                        L ${diagramData.size.width - borderWidth - innerRadius},${borderWidth} 
+                        Q ${diagramData.size.width - borderWidth},${borderWidth} ${diagramData.size.width - borderWidth},${borderWidth + innerRadius} 
                         L ${diagramData.size.width - borderWidth},50 
                         L ${borderWidth},50 Z`);
 
